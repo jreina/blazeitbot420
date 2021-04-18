30384269dcf657f5b2395d818a906a1600d1fefc
Johnny Reina <johnny.r.reina@gmail.com>
2021-04-18 17:35:59 -0500

    Move mongo factory

--- src/data/MongoFactory.js
+++
@@ -1,7 +0,0 @@
-const { MongoClient } = require("mongodb");
-class MongoFactory {
-  async getInstance() {
-    if (this._db) return this._db;
-    this._db = MongoClient.connect(process.env.MONGO_CONNECTION_STRING);
-  }
-}
